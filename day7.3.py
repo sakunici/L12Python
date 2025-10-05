4012d1a15a669908dea6e73f126130d446ea7ec0
sakunici <188367143+sakunici@users.noreply.github.com>
2025-10-05 17:38:13 +0700

    Lucky Draw

--- day7.3.py
+++ day7.3.py
@@ -1,8 +1,9 @@
-# Guest Verification
+# Guest Verification and Lucky Draw
 import random
 guest_list = ["Manee", "Piti", "Choojai", "Mana"]
 
 guest_choice=random.choice (guest_list)
+lucky_guest=random.choice (guest_list)
 guest_register_str=input("What is your name? : ")
 guest_input=str(guest_register_str)
 
@@ -10,8 +11,9 @@ if guest_input in guest_list:
     guest_list.remove(guest_input)
     print(f"Welcome {guest_input}! you have been checked in.")
     print(f"Guest still waiting {guest_list}")
+    if guest_input == lucky_guest: #Check if you are the Lucky Person
+        print (f"The winner is {guest_choice} !!! Congratulations ")
+    else:print("Thank you for coming")
 else:
     print(f"Sorry {guest_input}! you are not in the guest list.")
-
-
-
+    
