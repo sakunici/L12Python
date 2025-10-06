312c7645f59bec6fe1820bf016783b735e0de238
sakunici <188367143+sakunici@users.noreply.github.com>
2025-10-06 21:58:15 +0700

    for loops

---
+++ day8.py
@@ -0,0 +1,12 @@
+#For Loops การวนซ้ำเมื่อรู้จำนวนรอบ
+fruits=["apple","banana","orange","coconut","grape"]
+for fruit in fruits:
+    print(f"ฉันชอบกิน {fruit}")
+
+for i in range(1,7): # วนลูป 1-7
+    print(f"นี่คือรอบที่ {i}")
+
+#multiplication table
+tables = [1,2,3,4,5,6,7,8,9,10,11,12]
+for table in tables:
+    print(f" 3 x {table}")
