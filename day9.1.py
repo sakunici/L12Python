d7a5cd2c992f788d2ba24b1ac19a8fb955c30425
sakunici <188367143+sakunici@users.noreply.github.com>
2025-10-07 22:48:05 +0700

    create dictionaries in list and use for loop

---
+++ day9.1.py
@@ -0,0 +1,10 @@
+fruits_list = [
+    {"fruit":"banana","price":60,"colour":"yellow"},
+    {"fruit":"grape","price":80,"colour":"purple"},
+    {"fruit":"orange","price":100,"colour":"orange"},
+    {"fruit":"coconut","price":50,"colour":"green"},
+    {"fruit":"cherry","price":500,"colour":"red"},
+]
+for fruit_data in fruits_list:
+    print(f" My favourite fruit is {fruit_data['fruit']}, it cost {fruit_data['price']} THB it is {fruit_data['colour']}")
+
