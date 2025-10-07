ec679f5358b8841917012fef5cd192b5cecd8340
sakunici <188367143+sakunici@users.noreply.github.com>
2025-10-07 22:50:46 +0700

    add comment in the code

--- day9.1.py
+++ day9.1.py
@@ -1,10 +1,10 @@
-fruits_list = [
+fruits_list = [ # create the list if you have more than 1 things
     {"fruit":"banana","price":60,"colour":"yellow"},
     {"fruit":"grape","price":80,"colour":"purple"},
     {"fruit":"orange","price":100,"colour":"orange"},
     {"fruit":"coconut","price":50,"colour":"green"},
     {"fruit":"cherry","price":500,"colour":"red"},
 ]
-for fruit_data in fruits_list:
-    print(f" My favourite fruit is {fruit_data['fruit']}, it cost {fruit_data['price']} THB it is {fruit_data['colour']}")
+for fruit_data in fruits_list: # using For Loop to pick up one by one
+    print(f" My favourite fruit is {fruit_data['fruit']}, it cost {fruit_data['price']} THB it is {fruit_data['colour']}") # order to pick up one by one with all data of each fruit
 
