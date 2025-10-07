c4098097c9daa1a0552d6830f4a33f2dbdd1084f
sakunici <188367143+sakunici@users.noreply.github.com>
2025-10-07 22:31:51 +0700

    day9_dictionaries

---
+++ day9.py
@@ -0,0 +1,21 @@
+my_cat = {
+    "name": "เจ้าส้ม",
+    "age": 2,
+    "color": "Orange"
+}
+print(f"แมวของฉันชื่อ {my_cat['name']} และมีสี {my_cat['color']}")
+
+my_name = {
+    "name1" : "Miaou",
+    "age1": 39,
+    "city": "Bangkok",
+    "hobby1":"Python"
+}
+print(f"My name is {my_name['name1']} I'm {my_name['age1']} years old I live in {my_name['city']} my hobby is {my_name['hobby1']}")
+
+my_study ={
+    "lesson": "Python",
+    "day": 14,
+    "favourite": True
+}
+print(f"My lesson is {my_study['lesson']} I will continue learning for {my_study['day']} days and of course I do like it, {my_study['favourite']}")
