14f65c4343b17dd87b05e8646d8f992c3ef10979
sakunici <188367143+sakunici@users.noreply.github.com>
2025-10-11 21:08:57 +0700

    function

---
+++ day11.1.py
@@ -0,0 +1,4 @@
+def calculate_bmi(w,h):
+    return(w/h)
+result= calculate_bmi(55,(154**2))
+print(result)
---
+++ day11.py
@@ -0,0 +1,10 @@
+# Day 11: Functions - สร้างเครื่องมือของคุณเอง
+def greet(user_name):
+    print(f"Hello {user_name}")
+
+greet("Sunny")
+
+def add_numbers(a,b):
+    return a+b
+result=add_numbers(10,5)
+print(result)
