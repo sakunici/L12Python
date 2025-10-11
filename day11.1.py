9a188bf3873cb09c16081559cc0735bde9f887fd
sakunici <188367143+sakunici@users.noreply.github.com>
2025-10-11 21:48:07 +0700

    Practice Day11-PY

--- day11.1.py
+++ day11.1.py
@@ -1,4 +1,10 @@
-def calculate_bmi(w,h):
-    return(w/h)
-result= calculate_bmi(55,(154**2))
+
+w_str=input("Your Weight (kg): ")
+w_float=float(w_str)
+h_str=input("Your Height (m): ")
+h_float=float(h_str)
+
+def calculate_bmi(w_float,h_float):
+    return(w_float/(h_float**2))
+result= calculate_bmi(w_float,h_float)
 print(result)
