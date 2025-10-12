5007eb6f59f379eb6cc8938cdc8468513d046cc6
sakunici <188367143+sakunici@users.noreply.github.com>
2025-10-12 19:52:20 +0700

    day12string

--- day12.py
+++ day12.py
@@ -1,3 +1,9 @@
 # Day 12: การจัดการ String เพิ่มเติม
-Sentence_str=input("Type your sentense here : ")
-print(f"{Sentence_str.lower}")
+type_str=input("Type your sentense here : ")
+type_lower=type_str.lower()
+print(f" typer in lower letter is : {type_lower}")
+
+text=input("Type your text: ")
+text_cap=text.upper()
+print (f"Your text in Upper is: {text_cap}")
+
