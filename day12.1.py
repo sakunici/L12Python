86a3c1bef6388d2b239a87f116fa442520656dba
sakunici <188367143+sakunici@users.noreply.github.com>
2025-10-12 20:08:43 +0700

    replace

--- day12.1.py
+++ day12.1.py
@@ -2,3 +2,16 @@ text_input="    str123th  "
 text_clean=text_input.strip()
 print(f" '{text_clean}'")
 
+
+phone_number = "099-888-9999"
+# เราต้องการเก็บเบอร์โทรแบบไม่มีขีด
+cleaned_phone = phone_number.replace("-", "") # แทนที่ "-" ด้วย "ความว่างเปล่า"
+
+print(f"เบอร์โทรดั้งเดิม: {phone_number}")
+print(f"เบอร์โทรที่จัดรูปแบบใหม่: {cleaned_phone}")
+
+department="Department"
+clean_dept=department.replace ("ment","ttt" )
+
+print(f"Old text: {department}")
+print(f"New text: {clean_dept}")
