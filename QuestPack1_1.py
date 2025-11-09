c772ffb5413ab0d1ae9bfdec2c8143664e9a35c2
sakunici <188367143+sakunici@users.noreply.github.com>
2025-11-09 15:27:45 +0700

    pack1-1

---
+++ QuestPack1_1.py
@@ -0,0 +1,10 @@
+yourtext_str=input("Type your text in English: ") #รับค่าประโยคภาษาอังกฤษ
+text=yourtext_str.lower()
+vovel_str=["a","e","i","o","u"]
+count = 0
+for letter_vovel_str in text:
+    if letter_vovel_str in vovel_str:
+        count = count + 1
+print (f"You text have {count} vovel in total")
+
+
