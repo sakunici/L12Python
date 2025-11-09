6520f6c64005ac108c3b8517be2bf955ffb746c0
sakunici <188367143+sakunici@users.noreply.github.com>
2025-11-09 15:42:44 +0700

    Reverse

--- QuestPack1_1.py
+++ QuestPack1_1.py
@@ -1,3 +1,4 @@
+#นับจำนวนสระ (a, e, i, o, u) ทั้งตัวพิมพ์เล็กและใหญ่
 yourtext_str=input("Type your text in English: ") #รับค่าประโยคภาษาอังกฤษ
 text=yourtext_str.lower()
 vovel_str=["a","e","i","o","u"]
@@ -6,5 +7,4 @@ for letter_vovel_str in text:
     if letter_vovel_str in vovel_str:
         count = count + 1
 print (f"You text have {count} vovel in total")
-
 
---
+++ QuestPack1_2.py
@@ -0,0 +1,4 @@
+#โปรแกรมกลับคำ
+text_str=input("Type your text in English: ")
+string_var=text_str[: : -1]
+print (f" Your reversed text is {string_var}")
