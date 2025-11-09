15eb5f8ec5497201de65032c169fcf7a04cc5ed1
sakunici <188367143+sakunici@users.noreply.github.com>
2025-11-09 16:51:59 +0700

    address format

---
+++ QuestPack1_6.py
@@ -0,0 +1,6 @@
+# จัดฟอร์แมตที่อยู่
+house=input("Your house no.: ")
+road=input("Your road: ")
+subdis=input("Your sub-disctric: ")
+province=input("Your province: ")
+print(f"Your address is No. {house},{road} Road,{subdis},{province}")
